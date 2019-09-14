99b03ec80d576c95a2676d25b6879dbe8bf5c676
Renel Homere <rhomere@gmail.com>
2019-09-14 09:54:32 -0400

    add ContentTemplate UserControl

---
+++ WPF/Views/UserControls/Inspections/ContentTemplate.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF.Views.UserControls.Inspections
+{
+    /// <summary>
+    /// Interaction logic for ContentTemplate.xaml
+    /// </summary>
+    public partial class ContentTemplate : UserControl
+    {
+        public ContentTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
